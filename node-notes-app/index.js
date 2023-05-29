584c6c556ce0f90ae81305f1064f4bc9decd44aa
FIRST_NAME LAST_NAME <shabazbazigar@gmail.com>
2023-05-29 13:05:17 +0530

    Phonebook backend

--- node-notes-app/index.js
+++ node-notes-app/index.js
@@ -39,7 +39,7 @@ let notes = [
 
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    notes = notes.findInd(note => note.id !== id)
   
     response.status(204).end()
   })
---
+++ practive/phonebook-backend/index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const persons = require('./data')
+
+app.use(express.json());
+
+app.get('/', (request,response) => {
+    response.send('<h1>Hello world!</h1>')
+})
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request,response) => {
+    const currentTime = new Date().toString()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p> <br> Request received at: ${currentTime}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person){
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+    
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0
+    return maxId + 1
+  }
+
+app.post('/api/persons', (request, response)=> {
+    const body = request.body
+    const {name, number} = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'name or number missing' 
+      })
+    }
+
+    const existingPerson = persons.find(person => person.name === name)
+    if (existingPerson){
+        return response.status(400).json({ 
+            error: 'Name already exists' 
+          })
+    }
+
+  
+    const newPerson = {
+      id: generateId(),
+      name: body.name,
+      number: body.number      
+    }
+  
+    persons.push(newPerson)
+  
+    response.json(newPerson)
+
+})
+
+
+app.delete('/api/persons/:id', (request,response) => {
+    const id = Number(request.params.id)
+    const index = persons.findIndex(person => person.id === id)
+    
+    if (index !== -1){
+        persons.splice(index, 1)
+        response.sendStatus(204) 
+    } else {
+        response.status(404) 
+    }
+   
+})
+
+
+const PORT = 5000
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
